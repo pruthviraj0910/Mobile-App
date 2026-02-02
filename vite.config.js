71c0207d9f1cad76ce86cdf57d7c799cd7b4d472
pruthviraj0910 <pruthvirajreddy62@gmail.com>
2026-02-02 12:24:05 +0530

    Fix vite config and scripts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'wite'
-import react from '@witejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://witejs.web.app/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
 })
